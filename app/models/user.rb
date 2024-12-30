bcb354eb34f1b48aa4fec80b5140666b787db6ba
Wesley Lopes dos Reis <lopeswesley1392@gmail.com>
2024-12-30 14:14:33 -0300

    ajustando o cadastro de usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  validates :email, presence: true
   has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
